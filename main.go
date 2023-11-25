8631a1bf3f2f3611ecb2254725834231414afa09
bom.ph@finema.co <bom.ph@finema.co>
2023-11-25 15:50:17 +0700

    features: bom_23.11.21 - Add mongoDB config

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bom_proj_go/cmd/users"
+	"bom_proj_go/internal/database"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
+	database.InitializeDB()
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
@@ -13,6 +16,8 @@ func main() {
 			"message": "Server is running",
 		})
 	})
+
+	http.HandleFunc("/create-user", users.CreateUser)
 
 	err := r.Run()
 	if err != nil {
