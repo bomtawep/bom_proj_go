1d91e03e9f41707296d3f5aa951cc81e3cf64832
bom.ph@finema.co <bom.ph@finema.co>
2024-04-15 14:37:08 +0700

    features: bom_24.3.29 Add middleware - Add middleware - Change go version docker image

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"bom_proj_go/pkg/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"strconv"
 )
 
@@ -19,6 +20,12 @@ func main() {
 
 	// Create a new Fiber app
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "*",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "*",
+	}))
 	routes.GroupRoute(app)
 	err := app.Listen(`:` + strconv.Itoa(env.Port))
 	if err != nil {
